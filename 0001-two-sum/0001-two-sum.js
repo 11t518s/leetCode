78dfaf3a6e41ce4d24aa25e64abb8646a4dfa4c5
최봉수(derrick) <69836010+11t518s@users.noreply.github.com>
2022-12-19 16:41:56 +0900

    Time: 475 ms (5.03%), Space: 69.8 MB (5.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,20 +7,14 @@ const twoSum = (nums, target) => {
     let answer = []
     let prevNums = [] 
     nums.forEach((num, numIndex) => {
-        
-
         prevNums.some((prevNum, prevNumIndex) => {
-
             const isSame = prevNum + num === target
             if(!isSame) return;
             answer = [prevNumIndex, numIndex]
 
         })
         prevNums = [...prevNums, num]
-       
-
+        return
     })
     return answer
-    
-    
 };
