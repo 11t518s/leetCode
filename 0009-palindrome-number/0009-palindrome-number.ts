0549c5cb3ba634c8be3f9f5bff0548db297b39c6
최봉수(derrick) <69836010+11t518s@users.noreply.github.com>
2022-12-23 14:49:54 +0900

    Time: 171 ms (96.01%), Space: 52.3 MB (26.14%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -3,8 +3,6 @@ function isPalindrome(x: number): boolean {
     const len = listX.length
     
     return !listX.some((num, numIndex)=> {
-        console.log(num)
-        console.log(listX[len - numIndex - 1])
         return num !== listX[len - numIndex - 1]
     })
 };
