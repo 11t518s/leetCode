e48f2fcc213b53132f1317c8d54cab7bb717d287
최봉수(derrick) <69836010+11t518s@users.noreply.github.com>
2023-01-11 19:34:30 +0900

    Time: 67 ms (74.65%), Space: 41.8 MB (83.13%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -6,8 +6,6 @@
 var searchInsert = function(nums, target) {
     const before = nums.findIndex((num) => num <= target)
     const after = nums.findIndex((num) => num >= target)
-    console.log(before)
-    console.log(after)
     
-    return before === after ? before :after === -1 ? nums.length : after
+    return before === after ? before : after === -1 ? nums.length : after
 };
